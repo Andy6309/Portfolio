85164df59a60235aefbbd9a609e9220b551cf7cf
Andy6309 <andy_mize@yahoo.com>
2025-04-12 20:43:16 -0400

    add importorder-nortek as projectcard

--- src/config.js
+++ src/config.js
@@ -154,7 +154,7 @@ export const resume = "https://drive.google.com/file/d/1ZtrY8t5xHpC2OfkSfjE_o4xE
  ************************************************************** 
   List the repo names (string - "your-repo-name") you want to include (they will be sorted alphabetically). If empty, only the first 3 will be included.
 */
-export const filteredProjects = ["geo-map-app", "Cam3DtoSQL", "post-git"];
+export const filteredProjects = ["geo-map-app", "Cam3DtoSQL", "ImportOrder-Nortek"];
 
 // Replace the default GitHub image for matching repos below (images imported above - lines 7-8)
 export const projectCardImages = [
